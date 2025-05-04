b813de1387c1ff647a3e1a92843b31067242e925
Mouhammad Ndour <ndourmouhammad15@gmail.com>
2025-05-04 07:44:17 +0000

    Hello world msg

---
+++ src/cfehome/views.py
@@ -0,0 +1,21 @@
+import pathlib
+from django.shortcuts import render
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+
+
+def home_page_view(request ,*args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = "My Page"
+    my_contexte = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": (page_qs.count() * 100.0) / qs.count(),
+        "total_visit_count": qs.count(),
+    }
+    html_template = "home.html"
+    PageVisit.objects.create() 
+    return render(request, html_template, my_contexte)
